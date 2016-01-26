4306a43fd80b30f4306a2d622ec855f80be0cf96
yourecommerce <quan@yourecommerce.com>
2016-01-26 15:30:55 -0800

    Initial Checkint

---
+++ ScoreBandTest/ScoreBandTest/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScoreBandTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var score = 657;
+            Console.WriteLine(((score % 20) == 0 ? score : score + 20 - (score % 20)));
+
+            //Console.WriteLine(score / 10 * 10 + (score / 10 % 2) * 10);
+
+            score = 634;
+            Console.WriteLine("Score: " + score);
+            Console.WriteLine(((score % 20) == 0 ? score : score + 20 - (score % 20)));
+
+            score = 500;
+            Console.WriteLine("Score: " + score);
+            Console.WriteLine(((score % 20) == 0 ? score : score + 20 - (score % 20)));
+
+
+            score = 540;
+            Console.WriteLine("Score: " + score);
+            Console.WriteLine(((score % 20) == 0 ? score : score + 20 - (score % 20)));
+
+            score = 550;
+            Console.WriteLine("Score: " + score);
+            Console.WriteLine(((score % 20) == 0 ? score : score + 20 - (score % 20)));
+
+
+            score = 700;
+            Console.WriteLine("Score: " + score);
+            //Console.WriteLine(score / 10 * 10 + (score / 10 % 2) * 10);
+            Console.WriteLine(((score % 20) == 0 ? score : score + 20 - (score % 20)));
+            score = 701;
+            Console.WriteLine("Score: " + score);
+
+            Console.WriteLine(((score % 20) == 0 ? score : score + 20 - (score % 20)));
+            Console.ReadLine();
+        }
+    }
+}
